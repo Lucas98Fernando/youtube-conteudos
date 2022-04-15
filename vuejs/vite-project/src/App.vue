4c10efbc37adb60a6dd886332cec8de241aa8a07
Lucas Fernando <lucas98fernando@gmail.com>
2022-04-15 00:23:57 -0300

    Conteúdo do youtube para o pinia

--- vuejs/vite-project/src/App.vue
+++ vuejs/vite-project/src/App.vue
@@ -1,21 +1,27 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Home from "./components/Home.vue";
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <img alt="Vue logo" src="./assets/pinia.svg" width="120" />
+  <Home msg="Pinia" />
 </template>
 
 <style>
+body {
+  background-color: #2c3e50;
+  height: 100vh;
+  color: #f5f5f5;
+  font-family: sans-serif;
+}
+h1 {
+  font-size: 3rem;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
---
+++ vuejs/vite-project/src/components/Home.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { mapActions, storeToRefs } from "pinia";
+import { ref } from "vue";
+import { useMainStore } from "../store/main";
+
+defineProps<{ msg: string }>();
+
+const main = useMainStore();
+
+const { counter } = storeToRefs(main);
+
+const { increment } = mapActions(useMainStore, ["increment"]);
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <button type="button" @click="increment">
+    Valor do contador: {{ counter }}
+  </button>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
